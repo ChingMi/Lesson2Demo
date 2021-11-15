31b30a46749913ef700e5fb9b1b16af89f905635
GoldenRice <bluesky20242@gmail.com>
2021-11-15 19:15:03 +0800

    Initial Commit

---
+++ Lesson2Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2Demo
+//
+//  Created by 黃靖蜜 on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
